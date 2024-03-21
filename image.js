ba014daabf3f0109fb75f8f3bf53a2a1e115ac36
fozabo <128290023+fozabo@users.noreply.github.com>
2024-03-21 18:12:16 +0200

    Add files via upload

---
+++ image.js
@@ -0,0 +1,35 @@
+var app = angular.module('imageApp', []);
+
+app.controller('ImageController', function($http) {
+  var vm = this;
+
+  vm.images = [];
+  vm.currentIndex = null;
+
+  // Function to fetch images from Picsum Photos API
+  vm.fetchImages = function() {
+    $http.get('https://picsum.photos/v2/list?page=3&limit=50')
+      .then(function(response) {
+        // Success callback
+        // Map the response to format { thumbnail: '', fullsize: '' }
+        vm.images = response.data.map(function(img) {
+          var urlBase = 'https://picsum.photos/id/' + img.id;
+          return {
+            thumbnail: urlBase + '/50/50',
+            fullsize: urlBase + '/300/300'
+          };
+        });
+      }, function(error) {
+        // Error callback
+        console.error('Error fetching images:', error);
+      });
+  };
+
+  // Function to show full-size image when a thumbnail is clicked
+  vm.showFullSize = function(index) {
+    vm.currentIndex = index;
+  };
+
+  // Fetch images when controller initializes
+  vm.fetchImages();
+});
